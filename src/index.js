8f29548039e1c94fc271cf54a1514bfeeef4ce21
Toritoriri <kuligina.vika@bk.ru>
2023-08-01 09:40:13 +0500

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) {
+       n =  n * -1
+    }
+  let numToStr = n.toString();
+  let strToArray = numToStr.split("");
+  let arrayRev = strToArray.reverse().join("");
+return arrayRev
 }
